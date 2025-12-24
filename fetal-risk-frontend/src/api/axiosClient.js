480d1487e0618c0669c6a31dccb6e99925f4c3d3
Shreyas Kumar PM <juttuk324@gmail.com>
2025-12-24 12:29:30 +0530

    Configure axios client for production API

--- fetal-risk-frontend/src/api/axiosClient.js
+++ fetal-risk-frontend/src/api/axiosClient.js
@@ -1,15 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosClient;
