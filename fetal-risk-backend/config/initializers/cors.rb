ee1e9013b18f45ad1906fdb89d8ab858daff7891
Shreyas Kumar PM <juttuk324@gmail.com>
2025-12-24 12:37:30 +0530

    Fix CORS for CRA and Render frontend

--- fetal-risk-backend/config/initializers/cors.rb
+++ fetal-risk-backend/config/initializers/cors.rb
@@ -1,16 +1,16 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-      "http://localhost:3000",
-      "http://localhost:3001",
-      /https:\/\/.*\.netlify\.app/,
-      /https:\/\/.*\.onrender\.com/
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'https://skfetal-risk-frontend.onrender.com'
     )
 
-    resource "*",
+    resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
-             expose: ["Authorization"],
-             credentials: true
+             expose: ['Authorization'],
+             credentials: false
   end
 end
